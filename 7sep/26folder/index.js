e1d88f857887e82ac20290239b8e4f926ddd048e
joe <joe.recipe@gmail.com>
2022-10-06 11:34:31 +0200

    added

---
+++ 7sep/26folder/index.js
@@ -0,0 +1,19 @@
+function showAlertFronInput(){
+
+    let input = document.getElementById('message-box-input');
+    let message = input.value;
+    let div = document.getElementById('output')
+    div.innerHTML += "<div>message</div>"
+    let input1 = document.getElementById('message-box-input1');
+    let message1 = input.value;
+    let div1 = document.getElementById('output')
+    div.innerHTML += "<div>message</div>"
+    
+        
+    
+
+    }
+
+        
+    
+ 
